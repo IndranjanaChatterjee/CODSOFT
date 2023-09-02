c0f689eeffda82a9ffc18a3171781f036fa34cab
Indranjana <112919974+IndranjanaChatterjee@users.noreply.github.com>
2023-09-02 08:10:52 +0530

    Add files via upload

---
+++ portfolio/script.js
@@ -0,0 +1,9 @@
+var typed = new Typed("#element", {
+  strings: [
+    "Passionate Web developer","Competitive Coder","Coding Enthusiast",
+  ],
+  typeSpeed: 100,
+  backSpeed:100,
+  backDelay:1000,
+  loop:true
+});
