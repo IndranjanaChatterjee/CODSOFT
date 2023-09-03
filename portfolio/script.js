ea852b9a35fa29fb545f8b6859677187786c0602
Indranjana Chatterjee <indranjanachatterjee@gmail.com>
2023-09-03 13:00:18 +0530

    nav

--- portfolio/script.js
+++ portfolio/script.js
@@ -13,3 +13,15 @@ window.addEventListener("scroll",()=>
   console.log(h);
   h.classList.toggle("scrolldwn",window.scrollY>0);
 })
+const open=document.querySelector(".main i");
+const close=document.querySelector(".close i");
+const menu=document.querySelector("ul");
+open.addEventListener("click",()=>
+{
+  menu.style.display="flex";
+  menu.style.top="0";
+})
+close.addEventListener("click",()=>
+{
+  menu.style.top="-100%";
+})
